08a094e67842d161a4316a1803542a08d71d501a
MoustaphaSarr2024 <mamadou-moustapha.sarr@epitech.eu>
2025-11-20 17:35:58 +0100

    mise en place du serveur

---
+++ server/src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const router = require('./routes');
+
+const app = express();
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use('/api', router);
+
+module.exports = app;
